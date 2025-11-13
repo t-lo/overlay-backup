f058dbbdf71e7af6b9e4ad7ea87586e64c609035
Thilo Fromm <thilofromm@microsoft.com>
2025-11-13 13:46:23 +0100

    ls.sh: better support of subdirectory backups
    Signed-off-by: Thilo Fromm <thilo.alexander@gmail.com>

--- ls.sh
+++ ls.sh
@@ -11,9 +11,12 @@ if netfs_needs_mounting "${NETFS_MOUNT}" ; then
   trap "umount_netfs '${NETFS_MOUNT}'" EXIT
   mount_netfs "${NETFS_URI}" "${NETFS_MOUNT}" "${NETFS_MOUNTOPTS}"
 fi
-echo " --- File list in backup images directory '${NETFS_MOUNT}/'"
+echo " --- File list in backup images directory '${BACKUP_IMAGES_DEST}/'"
 echo
-ls -1 "${NETFS_MOUNT}"/ | grep -v '.mount-check'
+ls -1 "${BACKUP_IMAGES_DEST}"/ | grep -v '.mount-check'
 echo
+echo " --- Backup usage"
+du --max-depth=1 -h "${BACKUP_IMAGES_DEST}"/
 echo " --- NETFS usage"
 df -h "${NETFS_MOUNT}/${UTIL_NETFS_MOUNTFLAG_FILE}"
+echo
